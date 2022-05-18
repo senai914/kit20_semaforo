52ce4a5231ba5786fa259b3f4b8ca6b9524e0081
kleber.ufu@gmail.com <kleber.ufu@gmail.com>
2022-05-18 21:52:37 +0000

    Project created

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=kit20_semaforo.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/kit20_semaforo.production.hex
